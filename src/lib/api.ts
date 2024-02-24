d70a8917c8ed5170a192c3533a16e324d0d5fb42
Johnatas Félix <johnatasfelix04@gmail.com>
2024-02-23 23:00:19 -0300

    ajustado a importação env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const VITE_BASE_URL_API = process.env.VITE_BASE_URL_API || "http://localhost:3000"
+const VITE_BASE_URL_API = import.meta.env.VITE_BASE_URL_API || "http://localhost:3000"
 
 export const api = axios.create({baseURL: VITE_BASE_URL_API});
